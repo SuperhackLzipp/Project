0e082edd0ddf4f5dad0c3c3669d186e6cb10fd14
Leon David Zipp <leondavidzipp@gmx.de>
2024-08-02 20:46:27 +0200

    refined variable types, adjusted donate functions, added comments

--- contracts/src/PartyPromises.sol
+++ contracts/src/PartyPromises.sol
@@ -2,35 +2,36 @@
 pragma solidity ^0.8.20;
 
 contract PartyPromises {
-
     // Struct for storing a promise donation
     struct PromiseDonation {
         bytes32 promiseTitle;
         uint256 amount;
     }
     // Struct for storing a donator
+
     struct Donator {
-        uint256             totalAmount;
-        PromiseDonation[]   promiseDonations; // list of promises the donator chooses to donate to
+        uint256 totalAmount;
+        mapping(bytes32 => uint256) promiseDonations; // list of promises the donator chooses to donate to
     }
     // Struct for storing a promise
+
     struct Promise {
         // bytes32     promiseTitle;
-        string      description;
-        bool        completed;
+        string description;
+        bool completed;
     }
 
     // events
-    event Received(address, uint);
+    event Received(address, uint256);
     event PromiseAdded(bytes32 _title, string _description, uint8 _priority);
     event PromiseCompleted(bytes32 _title);
     event TimeExpired();
 
     // state variables
-    address private immutable   owner; // owner of the contract
-    bytes32 public immutable    partyName; // name of the party
-    uint256 public immutable    creationTime; // time of creation (dd/mm/yyyy)
-    uint256 public immutable    expirationTime; // time of expiration (dd/mm/yyyy)
+    address private immutable owner; // owner of the contract
+    bytes32 public immutable partyName; // name of the party
+    uint256 public immutable creationTime; // time of creation (dd/mm/yyyy)
+    uint256 public immutable expirationTime; // time of expiration (dd/mm/yyyy)
     // Promise[] public            promises; // list of promises
     mapping(bytes32 => Promise) public promises; // mapping of promises
     mapping(address => Donator) public donators; // mapping of donators
@@ -64,18 +65,42 @@ contract PartyPromises {
     }
 
     // payable default functions
-    receive() external payable greaterZero {}
-
-    fallback() external payable greaterZero{}
-
-    // payable donation functions
-    function Donate() external payable greaterZero {
-        require(donors[msg.sender] == 0, "You are already a donor");
-
-        donors[msg.sender] = new Donator(msg.value, new PromiseDonation[](0));
+    receive() external payable greaterZero {
+        /* nothing */
     }
 
-    function DonateAnonymouslyNonRefundable() {}
+    fallback() external payable greaterZero {
+        /* nothing */
+    }
+
+    /**
+     * Function to allow addresses to donate to the party
+     * @param _totalAmount - total amount to donate
+     * @param _promiseTitles - list of promise titles to donate to
+     * @param _individualAmounts - list of individual amounts to donate to each promise
+     */
+    function Donate(uint256 _totalAmount, bytes32[] memory _promiseTitles, uint256[] memory _individualAmounts)
+        external
+        payable
+        greaterZero
+    {
+        require(donors[msg.sender] == 0, "You are already a donor");
+        require(
+            _promiseTitles.length == _individualAmounts.length, "Length of promise titles and amounts must be equal"
+        );
+
+        for (uint256 i = 0; i < _promiseTitles.length; i++) {
+            donators[msg.sender].promiseDonations[_promiseTitles[i]] = _individualAmounts[i];
+        }
+    }
+
+    /**
+     * Function to allow addresses to donate to the party anonymously
+     * NON-REFUNDABLE!!!
+     */
+    function DonateAnonymouslyNonRefundable() external payable greaterZero {
+        /* nothing */
+    }
 
     /**
      * Adds a promise to the list of promises
@@ -93,7 +118,7 @@ contract PartyPromises {
      * @param _title - title of the promise
      */
     function CompletePromise(bytes32 _title) public notExpired {
-        for (uint i = 0; i < promises.length; i++) {
+        for (uint256 i = 0; i < promises.length; i++) {
             if (promises[i].title == _title) {
                 promises[i].completed = true;
                 emit PromiseCompleted(_title);
@@ -106,7 +131,7 @@ contract PartyPromises {
      * Withdraws the adequate amount of funds from the contract for every promise completed
      * For every promise not completed, the funds will be sent back to the donators
      */
-    function HandlePromiseRewards() public isOwner isExpired{
+    function HandlePromiseRewards() public isOwner isExpired {
         if (checkAllPromisesCompleted() == true) {
             payable(owner).transfer(address(this).balance);
             return;
@@ -114,14 +139,12 @@ contract PartyPromises {
 
         uint256 balanceToWithdraw = 0;
         uint256 len = donators.length;
-        for (uint i = 0; i < len; i++) {
-            
-        }
+        for (uint256 i = 0; i < len; i++) {}
     }
 
     // helpers
     function checkAllPromisesCompleted() public view returns (bool) {
-        for (uint i = 0; i < promises.length; i++) {
+        for (uint256 i = 0; i < promises.length; i++) {
             if (!promises[i].completed) {
                 return false;
             }
