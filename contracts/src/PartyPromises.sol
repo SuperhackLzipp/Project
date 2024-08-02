db4f2022fb8c506f0b51e0268e73cda333f7c03a
Leon David Zipp <leondavidzipp@gmx.de>
2024-08-02 20:33:29 +0200

    Donate function && other function

--- contracts/src/PartyPromises.sol
+++ contracts/src/PartyPromises.sol
@@ -10,13 +10,12 @@ contract PartyPromises {
     }
     // Struct for storing a donator
     struct Donator {
-        address             donatorAddress;
         uint256             totalAmount;
         PromiseDonation[]   promiseDonations; // list of promises the donator chooses to donate to
     }
     // Struct for storing a promise
     struct Promise {
-        bytes32     promiseTitle;
+        // bytes32     promiseTitle;
         string      description;
         bool        completed;
     }
@@ -32,8 +31,9 @@ contract PartyPromises {
     bytes32 public immutable    partyName; // name of the party
     uint256 public immutable    creationTime; // time of creation (dd/mm/yyyy)
     uint256 public immutable    expirationTime; // time of expiration (dd/mm/yyyy)
-    Promise[] public            promises; // list of promises
-    Donator[] public            donators; // list of donators
+    // Promise[] public            promises; // list of promises
+    mapping(bytes32 => Promise) public promises; // mapping of promises
+    mapping(address => Donator) public donators; // mapping of donators
 
     // modifiers
     modifier notExpired() {
@@ -51,6 +51,11 @@ contract PartyPromises {
         _;
     }
 
+    modifier greaterZero() {
+        require(msg.value > 0, "Value must be greater than zero");
+        _;
+    }
+
     // constructor
     constructor(bytes32 _partyName, uint256 _expirationTime) {
         partyName = _partyName;
@@ -59,15 +64,18 @@ contract PartyPromises {
     }
 
     // payable default functions
-    receive() external payable {
-        require(msg.value > 0, "Must send some Ether");
-        emit Received(msg.sender, msg.value);
+    receive() external payable greaterZero {}
+
+    fallback() external payable greaterZero{}
+
+    // payable donation functions
+    function Donate() external payable greaterZero {
+        require(donors[msg.sender] == 0, "You are already a donor");
+
+        donors[msg.sender] = new Donator(msg.value, new PromiseDonation[](0));
     }
 
-    fallback() external payable{
-        require(msg.value > 0, "Must send some Ether");
-        emit Received(msg.sender, msg.value);
-    }
+    function DonateAnonymouslyNonRefundable() {}
 
     /**
      * Adds a promise to the list of promises
@@ -91,6 +99,23 @@ contract PartyPromises {
                 emit PromiseCompleted(_title);
                 return;
             }
+        }
+    }
+
+    /**
+     * Withdraws the adequate amount of funds from the contract for every promise completed
+     * For every promise not completed, the funds will be sent back to the donators
+     */
+    function HandlePromiseRewards() public isOwner isExpired{
+        if (checkAllPromisesCompleted() == true) {
+            payable(owner).transfer(address(this).balance);
+            return;
+        }
+
+        uint256 balanceToWithdraw = 0;
+        uint256 len = donators.length;
+        for (uint i = 0; i < len; i++) {
+            
         }
     }
 
