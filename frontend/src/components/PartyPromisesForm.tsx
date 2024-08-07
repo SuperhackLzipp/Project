73e002da9fb9acc501d406135b56ba5d79b0bcd0
LeondavidZipp <leondavidzipp@gmx.de>
2024-08-07 13:43:28 +0200

    further adjustments

--- frontend/src/components/PartyPromisesForm.tsx
+++ frontend/src/components/PartyPromisesForm.tsx
@@ -3,11 +3,12 @@ import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
-import { List, ListItem, ListItemText } from "@mui/material";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import { List, ListItem, ListItemText, Tooltip } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-// import "../styles/PartyPromisesForm.css";
+import "../styles/PartyPromisesForm.css";
 
 export const PartyPromisesForm: React.FC = () => {
     const [name, setName] = useState<string>("");
@@ -38,23 +39,31 @@ export const PartyPromisesForm: React.FC = () => {
     };
 
     return (
-        <div>
-            <TextField
-                required
-                id="filled-required-attester"
-                label="Party Name"
-                variant="filled"
-                name="attester"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
+        <Stack direction="column" spacing={1} padding={1} className="stack">
+            <Stack direction="row" spacing={1}>
+                <TextField
+                    required
+                    id="filled-required-attester"
+                    label="Party Name"
+                    variant="filled"
+                    name="attester"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    fullWidth
+                />
+                <Tooltip title="Upload the Promises">
+                    <IconButton type="submit" color="success">
+                        <FileUploadIcon />
+                    </IconButton>
+                </Tooltip>
+            </Stack>
             <PromisesList promises={promises} setPromises={setPromises} />
             <PromiseForm
                 newPromise={newPromise}
                 handleChange={handleChange}
                 handleAddPromise={handleAddPromise}
             />
-        </div>
+        </Stack>
     );
 };
 
@@ -122,43 +131,53 @@ const PromiseForm: React.FC<PromiseFormProps> = ({
     handleAddPromise,
 }) => {
     return (
-        <form onSubmit={handleAddPromise} className="promiseField">
-            <Stack direction="column" spacing={1}>
+        <form onSubmit={handleAddPromise} className="stack">
+            <Stack direction="column" spacing={1} padding={1}>
                 <Stack direction="row" spacing={1}>
-                    <TextField
-                        required
-                        id="filled-required-title"
-                        label="Title"
-                        variant="filled"
-                        name="title"
-                        value={newPromise.title}
-                        onChange={handleChange}
-                    />
-                    <TextField
-                        required
-                        id="filled-required-attester"
-                        label="Attester Public Address"
-                        variant="filled"
-                        name="attester"
-                        value={newPromise.attester}
-                        onChange={handleChange}
-                    />
+                    <Box flex={2}>
+                        <TextField
+                            required
+                            id="filled-required-title"
+                            className="titleField"
+                            label="Title"
+                            variant="filled"
+                            name="title"
+                            value={newPromise.title}
+                            onChange={handleChange}
+                            fullWidth
+                        />
+                    </Box>
+                    <Box flex={1}>
+                        <TextField
+                            required
+                            id="filled-required-attester"
+                            label="Attester Public Address"
+                            variant="filled"
+                            name="attester"
+                            value={newPromise.attester}
+                            onChange={handleChange}
+                            fullWidth
+                        />
+                    </Box>
                 </Stack>
                 <TextField
                     required
                     id="filled-multiline-static-description"
                     label="Description"
                     multiline
-                    rows={4}
+                    rows={10}
                     variant="filled"
                     name="description"
                     value={newPromise.description}
                     onChange={handleChange}
+                    fullWidth
                 />
-                <Box display="flex" justifyContent="center">
-                    <IconButton type="submit" color="success">
-                        <AddIcon />
-                    </IconButton>
+                <Box display="flex" justifyContent="right">
+                    <Tooltip title="Add Promise">
+                        <IconButton type="submit" color="success">
+                            <AddIcon />
+                        </IconButton>
+                    </Tooltip>
                 </Box>
             </Stack>
         </form>
